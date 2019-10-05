6ca267f4d7140a4b531287f983da70e9e1c5b967
muxiangqiu <1158025591@qq.com>
2019-10-05 14:04:40 +0800

    support read only transaction

--- packages/typeorm/src/node/annotation/transactional.ts
+++ packages/typeorm/src/node/annotation/transactional.ts
@@ -11,10 +11,11 @@ export interface TransactionalOption {
     name?: string;
     isolation?: IsolationLevel;
     propagation?: Propagation;
+    readOnly?: boolean;
 }
 export namespace TransactionalOption {
     export function is(option: any): option is TransactionalOption {
-        return option && (option.name !== undefined || option.isolation !== undefined);
+        return option && (option.name !== undefined || option.isolation !== undefined || option.readOnly !== undefined);
     }
 }
 
@@ -23,28 +24,31 @@ export interface TransactionalDecorator {
 }
 
 export const Transactional = <TransactionalDecorator>function (nameOrTransactionalOption?: string | TransactionalOption): MethodDecorator {
-    const { name, isolation, propagation } = getTransactionalOption(nameOrTransactionalOption);
+    const { name, isolation, propagation, readOnly } = getTransactionalOption(nameOrTransactionalOption);
     return (target: Object, methodName: string, descriptor: PropertyDescriptor) => {
         const originalMethod = descriptor.value;
-        descriptor.value = function (...args: any[]) {
+        descriptor.value = async function (...args: any[]) {
             if (propagation === Propagation.Required) {
-                if (OrmContext.getEntityManager(name)) {
+                const em = OrmContext.getEntityManager(name);
+                if (em && em.queryRunner && (em.queryRunner.isTransactionActive || readOnly)) {
                     return originalMethod.apply(this, args);
                 }
             }
-            const transactionCallback = (entityManager: EntityManager) => {
+            const callback = async (entityManager: EntityManager) => {
 
                 OrmContext.pushEntityManager(name!, entityManager);
                 try {
-                    return originalMethod.apply(this, args);
+                    return await originalMethod.apply(this, args);
                 } finally {
                     OrmContext.popEntityManager(name!);
                 }
             };
-            if (isolation) {
-                return getConnection(name).manager.transaction(isolation, transactionCallback);
+            if (readOnly) {
+                return await callback(getConnection(name).manager);
+            } else if (isolation) {
+                return getConnection(name).manager.transaction(isolation, callback);
             } else {
-                return getConnection(name).manager.transaction(transactionCallback);
+                return getConnection(name).manager.transaction(callback);
             }
         };
 
@@ -52,13 +56,17 @@ export const Transactional = <TransactionalDecorator>function (nameOrTransaction
 };
 
 export function getTransactionalOption(nameOrTransactionalOption?: string | TransactionalOption) {
-    let option: TransactionalOption = {};
+    const defaultOption = {
+        name: DEFAULT_CONNECTION_NAME,
+        propagation: Propagation.Required,
+        readOnly: false
+    };
+    let option: TransactionalOption = defaultOption;
     if (TransactionalOption.is(nameOrTransactionalOption)) {
-        option = { ...nameOrTransactionalOption };
+        option = { ...defaultOption, ...nameOrTransactionalOption };
     } else if (nameOrTransactionalOption) {
-        option = { name: nameOrTransactionalOption };
+        option = { ...defaultOption, name: nameOrTransactionalOption };
     }
-    option = { ...{ name: DEFAULT_CONNECTION_NAME, propagation: Propagation.Required }, ...option };
 
     return option;
 }
