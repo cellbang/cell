bb44b8ad28dc7fe0c8dba0424000a6756e2889e6
muxiangqiu <1158025591@qq.com>
2019-10-16 00:33:15 +0800

    add backend app template

--- dev-packages/cli/src/init/templates.ts
+++ dev-packages/cli/src/init/templates.ts
@@ -1,5 +1,6 @@
 export const templates: any = {
     'sample-app': '{{ templatePath }}/sample-app',
+    'backend-app': '{{ templatePath }}/backend-app',
     'multi-component': '{{ templatePath }}/multi-component',
     'business-framework': '{{ templatePath }}/multi-component'
 };
---
+++ dev-packages/cli/templates/backend-app/src/module.ts
@@ -0,0 +1,3 @@
+import './user-controller';
+import { autoBind } from '@malagu/core';
+export default autoBind();
