c25cfca0245de21557286151eeb2a34fecd4da25
linxiaowu66 <linguang661990@126.com>
2020-01-10 21:38:22 +0800

    fix(cli): make malaguCorePkgVersionConsistency more common

--- dev-packages/cli/src/util/index.ts
+++ dev-packages/cli/src/util/index.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 import * as lockfile from '@yarnpkg/lockfile';
 const chalk = require('chalk');
 
-export function malaguCorePkgVersionConsistency (projectPath: string) {
+export function checkPkgVersionConsistency (pkgName: string, projectPath: string) {
   const yarnLockFile = paths.resolve(projectPath, 'yarn.lock');
   const npmLockFile = paths.resolve(projectPath, 'package-lock.json');
 
@@ -11,16 +11,18 @@ export function malaguCorePkgVersionConsistency (projectPath: string) {
     const file = fs.readFileSync(yarnLockFile, 'utf8');
     const json = lockfile.parse(file);
     if (json.type !== 'success') {
-      throw new Error(chalk.red`yarn.lock file is in conflict status, pls solve the conflict then run the command again`);
+      console.log(chalk`malagu {red.bold error} - yarn.lock file is in conflict status, pls solve the conflict then run the command again`);
+      process.exit(1);
     }
     if (json.object) {
-      let malaguCoreVer = '';
-      Object.keys(json.object).forEach(pkgName => {
-        if (pkgName.match(/@malagu\/core/)) {
-          if (!malaguCoreVer) {
-            malaguCoreVer = json.object[pkgName].version;
+      let pkgVersion = '';
+      Object.keys(json.object).forEach(pkg => {
+        if (pkg.match(pkgName)) {
+          if (!pkgVersion) {
+            pkgVersion = json.object[pkg].version;
           } else {
-            throw new Error(chalk.red(`@malagu/core package has existed two different versions[${chalk.yellow(malaguCoreVer)} vs ${chalk.yellow(json.object[pkgName].version)}], thus will cause some unexpected problem, so pls check`));
+            console.log(chalk`malagu {red.bold error} - ${pkgName} package has existed two different versions[${chalk.yellow(pkgVersion)} vs ${chalk.yellow(json.object[pkg].version)}], thus will cause some unexpected problem, so pls check`);
+            process.exit(1);
           }
         }
       });
@@ -29,11 +31,13 @@ export function malaguCorePkgVersionConsistency (projectPath: string) {
     const json: { dependencies: { [key: string]: any}} = require(npmLockFile);
 
     Object.keys(json.dependencies).forEach((dep: string) => {
-      if (json.dependencies[dep].dependencies && json.dependencies[dep].dependencies['@malagu/core']) {
-        throw new Error(chalk.red(`@malagu/core package has existed two different versions[${chalk.yellow(dep)} has version ${chalk.yellow(json.dependencies[dep].dependencies['@malagu/core'].version)} of @malagu/core], thus will cause some unexpected problem, so pls check`));
+      if (json.dependencies[dep].dependencies && json.dependencies[dep].dependencies[pkgName]) {
+        console.log(chalk`malagu {red.bold error} - ${pkgName} package has existed two different versions[${chalk.yellow(dep)} has version ${chalk.yellow(json.dependencies[dep].dependencies[pkgName].version)} of ${pkgName}], thus will cause some unexpected problem, so pls check`);
+        process.exit(1);
       }
     });
   } else {
-    throw new Error(chalk.red`Pls generate lock file by Yarn or Npm firstly`);
+    console.log(chalk`malagu {red.bold error} - Pls generate lock file by Yarn or Npm firstly`);
+    process.exit(1);
   }
 }
