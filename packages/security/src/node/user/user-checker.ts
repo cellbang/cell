alibaba/malagu

feat(core:logger): logger component initial version

packages/security/src/node/user/user-checker.ts
@@ -1,32 +1,36 @@
+import { Logger as Log } from 'loglevel';
 import { User, UserChecker } from './user-protocol';
-import { Component, Autowired, Logger } from '@malagu/core';
+import { Component, Autowired, Logger, DefaultLogger } from '@malagu/core';
 import { LockedError, AccountExpiredError, DisabledError, CredentialsExpiredError } from '../error';
 
 @Component(UserChecker)
 export class UserCheckerImpl implements UserChecker {
-    @Autowired(Logger)
-    protected readonly logger: Logger;
+    private readonly _logger: Log;
+
+    constructor(@Autowired(Logger) protected readonly logger: DefaultLogger) {
+      this._logger = this.logger.child({ component: 'malagu:security:user:checker' });
+    }
     async check(user: User): Promise<void> {
         if (!user.accountNonLocked) {
-            this.logger.debug('User account is locked');
+            this._logger.debug('User account is locked');
 
             throw new LockedError('User account is locked');
         }
 
         if (!user.enabled) {
-            this.logger.debug('User account is disabled');
+            this._logger.debug('User account is disabled');
 
             throw new DisabledError('User is disabled');
         }
 
         if (!user.accountNonExpired) {
-            this.logger.debug('User account is expired');
+            this._logger.debug('User account is expired');
 
             throw new AccountExpiredError('User account has expired');
         }
 
         if (!user.credentialsNonExpired) {
-            this.logger.debug('User account is expired');
+            this._logger.debug('User account is expired');
 
             throw new CredentialsExpiredError('User credentials have expired');
         }

cellbang/cell

packages/security/src/node/user/user-checker.ts
@@ -6,27 +6,28 @@ import { LockedError, AccountExpiredError, DisabledError, CredentialsExpiredErro
 export class UserCheckerImpl implements UserChecker {
     @Autowired(Logger)
     protected readonly logger: Logger;
+
     async check(user: User): Promise<void> {
         if (!user.accountNonLocked) {
-            this.logger.debug('User account is locked');
+            this.logger.info('User account is locked');
 
             throw new LockedError('User account is locked');
         }
 
         if (!user.enabled) {
-            this.logger.debug('User account is disabled');
+            this.logger.info('User account is disabled');
 
             throw new DisabledError('User is disabled');
         }
 
         if (!user.accountNonExpired) {
-            this.logger.debug('User account is expired');
+            this.logger.info('User account is expired');
 
             throw new AccountExpiredError('User account has expired');
         }
 
         if (!user.credentialsNonExpired) {
-            this.logger.debug('User account is expired');
+            this.logger.info('User account is expired');
 
             throw new CredentialsExpiredError('User credentials have expired');
         }