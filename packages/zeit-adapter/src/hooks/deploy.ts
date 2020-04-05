1ea5c487057bea6c87c8655ae3aa028f84a2e38d
muxiangqiu <1158025591@qq.com>
2020-04-05 19:03:58 +0800

    fix(zeit-adapter) add bold style

--- packages/zeit-adapter/src/hooks/deploy.ts
+++ packages/zeit-adapter/src/hooks/deploy.ts
@@ -6,6 +6,6 @@ import { spawnSync } from 'child_process';
 export default (context: HookContext) => {
     const { pkg, prod } = context;
     const cwd = getHomePath(pkg);
-    console.log(`Deploying ${chalk.yellow(pkg.pkg.name)} to Zeit...`);
+    console.log(`Deploying ${chalk.bold.yellow(pkg.pkg.name)} to Zeit...`);
     spawnSync('now', prod ? ['--prod', '--local-config', join(cwd, 'now.json')] : ['--local-config', join(cwd, 'now.json')], { cwd, stdio: 'inherit' });
 };
