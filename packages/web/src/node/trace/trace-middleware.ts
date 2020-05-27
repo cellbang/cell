1f783e3690054c7b6c1ae2045ac98f6a01c68992
muxiangqiu <1158025591@qq.com>
2020-05-27 18:50:42 +0800

    fix(web) response trace id field

--- packages/web/src/node/trace/trace-middleware.ts
+++ packages/web/src/node/trace/trace-middleware.ts
@@ -1,7 +1,7 @@
 import { TraceIdResolver } from './trace-protocol';
 import { Middleware } from '../middleware';
 import { Context } from '../context';
-import { Component, Autowired, Logger } from '@malagu/core';
+import { Component, Autowired, Logger, Value } from '@malagu/core';
 import { TRACE_MIDDLEWARE_PRIORITY, TRACE_ID_RESPONSE_FIELD } from './trace-protocol';
 
 @Component(Middleware)
@@ -13,6 +13,9 @@ export class TraceMiddleware implements Middleware {
     @Autowired(Logger)
     protected readonly logger: Logger;
 
+    @Value(TRACE_ID_RESPONSE_FIELD)
+    protected readonly traceField: string;
+
     async handle(ctx: Context, next: () => Promise<void>): Promise<void> {
         const method = ctx.request.method;
         const path = ctx.request.path;
@@ -21,7 +24,7 @@ export class TraceMiddleware implements Middleware {
         const now = Date.now();
 
         Context.setTraceId(traceId);
-        ctx.response.setHeader(TRACE_ID_RESPONSE_FIELD, traceId);
+        ctx.response.setHeader(this.traceField, traceId);
         try {
             await next();
         } finally {
