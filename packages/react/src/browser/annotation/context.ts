511d1281a9f359694e90c7deb4e0b81ed1112f6f
muxiangqiu <muxiangqiu@gmail.com>
2020-06-29 23:18:54 +0800

    fix(react+material-ul) remove @Context id

--- packages/react/src/browser/annotation/context.ts
+++ packages/react/src/browser/annotation/context.ts
@@ -1,10 +1,9 @@
 import { ReactComponent } from './react-component';
-import { interfaces } from 'inversify';
 import { CONTEXT } from '../app/app-protocol';
 
 export const Context =
-    function (id?: interfaces.ServiceIdentifier<any> | interfaces.ServiceIdentifier<any>[], component?: any, rebind: boolean = false): (target: any) => any {
+    function (component?: any, rebind: boolean = false): (target: any) => any {
         return (t: any) => {
-            ReactComponent(id || CONTEXT, component || t, rebind)(t);
+            ReactComponent(CONTEXT, component || t, rebind)(t);
         };
     };
