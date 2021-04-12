a8a4c00a555d13a6cab7fb0f6ad9777c730177a4
lu_zuoye <luzuoye@i72.com>
2021-04-12 10:16:44 +0800

    feat(vue): add style webpack loaders

--- packages/vue/src/hooks/webpack.ts
+++ packages/vue/src/hooks/webpack.ts
@@ -1,28 +1,191 @@
 import { WebpackContext, ConfigurationContext } from '@malagu/cli-service';
+import { getFrontendMalaguConfig } from '@malagu/cli-common';
 
 export default async (context: WebpackContext) => {
     const { configurations } = context;
-    const config = ConfigurationContext.getFrontendConfiguration(configurations);
-    if (config) {
+    const webpackConfig = ConfigurationContext.getFrontendConfiguration(
+        configurations
+    );
+    if (webpackConfig) {
+        const isProd = process.env.NODE_ENV === 'production';
         const { VueLoaderPlugin } = require('vue-loader');
+        const { webpack } = getFrontendMalaguConfig(context.cfg);
 
-        config
-            .module
-                .rule('vue')
-                    .test(/\.vue$/)
-                    .use('vue-loader')
-                        .loader('vue-loader')
-                    .end()
-                .end()
-                .rule('css')
-                    .test(/\.css$/)
-                    .use('vue-style-loader')
-                        .loader('vue-style-loader')
-                    .end()
-                    .use('css-loader')
-                        .loader('css-loader');
-        config
-            .plugin('vueLoader')
-                .use(VueLoaderPlugin);
+        const sourceMap = !!webpack.devtool;
+        const defaultLoaderOptions = {
+            css: {},
+            scss: {},
+            sass: {},
+            less: {},
+            stylus: {},
+            postcss: {},
+        };
+        const loaderOptions = webpack.css || defaultLoaderOptions;
+        if (Object.keys(loaderOptions.postcss).length === 0) {
+            loaderOptions.postcss = {
+                postcssOptions: {
+                    plugins: [require('autoprefixer')],
+                },
+            };
+        }
+        const shadowMode = false;
+        const needInlineMinification = isProd;
+        const cssnanoOptions = {
+            preset: [
+                'default',
+                {
+                    mergeLonghand: false,
+                    cssDeclarationSorter: false,
+                },
+            ],
+        };
+        if (sourceMap) {
+            (cssnanoOptions as any).map = { inline: false };
+        }
+
+        // @ts-ignore
+        function createCSSRule(
+            lang: string,
+            test: RegExp,
+            loader?: string,
+            options = {}
+        ) {
+            const baseRule = webpackConfig!.module.rule(lang).test(test);
+
+            // rules for <style module>
+            const vueModulesRule = baseRule
+                .oneOf('vue-modules')
+                .resourceQuery(/module/);
+            applyLoaders(vueModulesRule, true);
+
+            // rules for <style>
+            const vueNormalRule = baseRule.oneOf('vue').resourceQuery(/\?vue/);
+            applyLoaders(vueNormalRule);
+
+            // rules for *.module.* files
+            const extModulesRule = baseRule
+                .oneOf('normal-modules')
+                .test(/\.module\.\w+$/);
+            applyLoaders(extModulesRule, true);
+
+            // rules for normal CSS imports
+            const normalRule = baseRule.oneOf('normal');
+            applyLoaders(normalRule);
+
+            function applyLoaders(rule: any, forceCssModule = false) {
+                rule.use('vue-style-loader')
+                    .loader(require.resolve('vue-style-loader'))
+                    .options({
+                        sourceMap,
+                        shadowMode,
+                    });
+
+                const cssLoaderOptions = Object.assign(
+                    {
+                        sourceMap,
+                        importLoaders:
+                            1 + // stylePostLoader injected by vue-loader
+                            1 + // postcss-loader
+                            (needInlineMinification ? 1 : 0),
+                    },
+                    loaderOptions.css
+                );
+
+                if (forceCssModule) {
+                    cssLoaderOptions.modules = {
+                        ...cssLoaderOptions.modules,
+                        auto: true,
+                    };
+                }
+
+                if (cssLoaderOptions.modules) {
+                    cssLoaderOptions.modules = {
+                        localIdentName: '[name]_[local]_[hash:base64:5]',
+                        ...cssLoaderOptions.modules,
+                    };
+                }
+
+                rule.use('css-loader')
+                    .loader(require.resolve('css-loader'))
+                    .options(cssLoaderOptions);
+
+                if (needInlineMinification) {
+                    rule.use('cssnano')
+                        .loader(require.resolve('postcss-loader'))
+                        .options({
+                            sourceMap,
+                            postcssOptions: {
+                                plugins: [require('cssnano')(cssnanoOptions)],
+                            },
+                        });
+                }
+
+                rule.use('postcss-loader')
+                    .loader(require.resolve('postcss-loader'))
+                    .options(
+                        Object.assign({ sourceMap }, loaderOptions.postcss)
+                    );
+
+                if (loader) {
+                    let resolvedLoader;
+                    try {
+                        resolvedLoader = require.resolve(loader);
+                    } catch (error) {
+                        resolvedLoader = loader;
+                    }
+
+                    rule.use(loader)
+                        .loader(resolvedLoader)
+                        .options(Object.assign({ sourceMap }, options));
+                }
+            }
+        }
+
+        webpackConfig!.module.noParse(
+            /^(vue|vue-router|vuex|vuex-router-sync)$/
+        );
+
+        webpackConfig!.module
+            .rule('vue')
+            .test(/\.vue$/)
+            .use('cache-loader')
+            .loader(require.resolve('cache-loader'))
+            .options({})
+            .end()
+            .use('vue-loader')
+            .loader(require.resolve('vue-loader'))
+            .options({})
+            .end()
+            .end();
+
+        createCSSRule('css', /\.css$/);
+        createCSSRule('postcss', /\.p(ost)?css$/);
+        createCSSRule(
+            'scss',
+            /\.scss$/,
+            'sass-loader',
+            Object.assign({}, loaderOptions.scss || loaderOptions.sass)
+        );
+        createCSSRule(
+            'sass',
+            /\.sass$/,
+            'sass-loader',
+            Object.assign({}, loaderOptions.scss || loaderOptions.sass)
+        );
+        createCSSRule('less', /\.less$/, 'less-loader', loaderOptions.less);
+        createCSSRule(
+            'stylus',
+            /\.styl(us)?$/,
+            'stylus-loader',
+            loaderOptions.stylus
+        );
+
+        webpackConfig.plugin('vue-loader').use(VueLoaderPlugin);
+        console.log('================');
+        console.log(
+            'webpackConfig.toString(  >>>>>>',
+            webpackConfig.toString()
+        );
+        console.log('================');
     }
 };
