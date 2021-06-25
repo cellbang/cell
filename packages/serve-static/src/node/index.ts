953934f95c981cd0374198ee9f97d9cbc9a1a391
wulinsheng123 <409187100@qq.com>
2021-06-25 09:14:33 +0800

    Refactor serve static (#31)
    Refactor serve static (#24)
    
    Co-authored-by: wuls <linsheng.wu@beantechs.com>

--- packages/serve-static/src/node/index.ts
+++ packages/serve-static/src/node/index.ts
@@ -1,1 +1,2 @@
+
 export * from './serve-static-middleware';
--- packages/serve-static/src/node/serve-static-middleware.ts
+++ packages/serve-static/src/node/serve-static-middleware.ts
@@ -1,15 +1,14 @@
 import { Middleware, Context, RequestMatcher } from '@malagu/web/lib/node';
-import * as serveStatic from 'serve-static';
+import {serveStatic, mime} from './serve-static';
 import { Value, Component, Autowired } from '@malagu/core';
 import { HTTP_MIDDLEWARE_PRIORITY } from '@malagu/web/lib/node';
 import { SERVER_PATH, HttpMethod, MediaType, HttpHeaders } from '@malagu/web';
 import { OutgoingMessage } from 'http';
-
 @Component(Middleware)
 export class ServeStaticMiddleware implements Middleware {
 
     @Value('malagu["serve-static"]')
-    protected config: { spa: boolean, root: string, path: string, apiPath: string, options: any };
+    protected config: { spa: boolean, root: string, path: string, apiPath: string, options: any};
 
     @Value(SERVER_PATH)
     protected path: string;
@@ -18,7 +17,9 @@ export class ServeStaticMiddleware implements Middleware {
     protected readonly requestMatcher: RequestMatcher;
 
     async handle(ctx: Context, next: () => Promise<void>): Promise<void> {
+        this.config.options.baseHref = this.path;
         const method = ctx.request.method;
+
         if (!(method === HttpMethod.GET || method === HttpMethod.HEAD) || ctx.request.query['static'] === 'skip') {
             await next();
             return;
@@ -41,17 +42,17 @@ export class ServeStaticMiddleware implements Middleware {
                     if (opts.headers) {
                         Object.keys(opts.headers).forEach(key => res.setHeader(key, opts.headers[key]));
                     }
-                    if ((serveStatic.mime as any).lookup!(path) === MediaType.TEXT_HTML) {
+                    if ((mime as any).lookup!(path) === MediaType.TEXT_HTML) {
                         // Custom Cache-Control for HTML files
                         res.setHeader(HttpHeaders.CACHE_CONTROL, `public, max-age=${opts.htmlMaxAge / 1000}`);
                     }
                 };
             }
+
             serveStatic(this.config.root, this.config.options)(ctx.request as any, ctx.response as any, ((err: any) => {
                 const url = ctx.request.url;
                 if (url !== '/index.html') {
                     if (!this.config.spa) {
-                        next().then(resolve).catch(reject);
                         return;
                     }
                     ctx.request.url = '/index.html';
@@ -61,7 +62,7 @@ export class ServeStaticMiddleware implements Middleware {
                 } else {
                     next().then(resolve).catch(reject);
                 }
-             }) as any);
+            }) as any);
         };
 
         return new Promise(executor);
