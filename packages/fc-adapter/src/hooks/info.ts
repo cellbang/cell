65ab864be45d7f0ce5eb3fd2911ffbab22866be9
Jaryn <xcanfo@126.com>
2021-10-05 22:21:35 +0800

    feat: info command support ali api gateway & timer tigger

--- packages/fc-adapter/src/hooks/info.ts
+++ packages/fc-adapter/src/hooks/info.ts
@@ -2,10 +2,11 @@ import { CliContext } from '@malagu/cli-service';
 import { Account, Credentials } from '@malagu/cloud';
 import { FaaSAdapterUtils, DefaultProfileProvider } from '@malagu/faas-adapter/lib/hooks';
 const FCClient = require('@alicloud/fc2');
-
+const CloudAPI = require('@alicloud/cloudapi');
 const chalk = require('chalk');
 
 let fcClient: any;
+let apiClient: any;
 
 export default async (context: CliContext) => {
 
@@ -17,7 +18,7 @@ export default async (context: CliContext) => {
     const { region, account, credentials } = await profileProvider.provide(adapterConfig);
     await createClients(adapterConfig, region, credentials, account);
 
-    const { service, trigger } = adapterConfig;
+    const { service, trigger, apiGateway } = adapterConfig;
     const functionMeta = adapterConfig.function;
     const serviceName = service.name;
     const functionName = functionMeta.name;
@@ -40,12 +41,51 @@ export default async (context: CliContext) => {
         }
     }
 
-    if (trigger.triggerType === 'http') {
+    if (apiGateway) {
         console.log(chalk`\n{bold.cyan - API Gateway:}`);
+        const { api } = apiGateway;
+        const apiGatewayList = await getApiGatwayList(api);
+        if (apiGatewayList.length > 1) {
+            throw new Error(`There are two or more apis named [${api.name}] in the api gateway`);
+        } else if (apiGatewayList.length === 1) {
+            const apiId = apiGatewayList[0].ApiId;
+            const apiGatewayInfo = await getApiGatwayInfo(apiId);
+            console.log(`    - ApiName : ${apiGatewayInfo.ApiName}`);
+            console.log(`    - ServiceProtocol : ${apiGatewayInfo.ServiceConfig.ServiceProtocol}`);
+            console.log(`    - ServiceTimeout : ${apiGatewayInfo.ServiceConfig.ServiceTimeout}`);
+            console.log(`    - ServiceAddress : ${apiGatewayInfo.ServiceConfig.ServiceAddress}`);
+            console.log(`    - ServiceHttpMethod : ${apiGatewayInfo.ServiceConfig.ServiceHttpMethod}`);
+            console.log(`    - Visibility : ${apiGatewayInfo.Visibility}`);
+            console.log(`    - ApiId : ${apiGatewayInfo.ApiId}`);
+        } else {
+            console.log('No ApiGateway Found');
+        }
+    }
+
+    if (trigger && trigger.triggerType === 'http') {
+        console.log(chalk`\n{bold.cyan - Trigger:}`);
         console.log(`    - Methods: ${trigger.triggerConfig.methods}`);
         console.log(chalk`    - Url: ${chalk.green.bold(
             `https://${account.id}.${region}.fc.aliyuncs.com/2016-08-15/proxy/${serviceName}.${trigger.qualifier}/${functionName}/`)}`);
 
+    } else if (trigger && trigger.triggerType === 'timer') {
+        console.log(chalk`\n{bold.cyan - Trigger:}`);
+        const tiggerInfo = await fcClient.listTriggers(serviceName, functionName);
+        tiggerInfo.data.triggers.forEach((item: any) => {
+            console.log(`    - triggerName: ${item.triggerName}`);
+            console.log(`    - qualifier: ${item.qualifier}`);
+            console.log(`    - triggerType: ${item.triggerType}`);
+            console.log(`    - cron: ${item.triggerConfig.cronExpression}`);
+            console.log(`    - enable: ${item.triggerConfig.enable}`);
+        });
+    } else {
+        console.log(chalk`\n{bold.cyan - Trigger:}`);
+        const tiggerInfo = await fcClient.listTriggers(serviceName, functionName);
+        tiggerInfo.data.triggers.forEach((item: any) => {
+            console.log(`    - triggerName: ${item.triggerName}`);
+            console.log(`    - qualifier: ${item.qualifier}`);
+            console.log(`    - triggerType: ${item.triggerType}`);
+        });
     }
 
     console.log();
@@ -65,5 +105,27 @@ async function createClients(adapterConfig: any, region: string, credentials: Cr
         internal: adapterConfig.internal
     });
 
+    apiClient = new CloudAPI({
+        accessKeyId: credentials.accessKeyId,
+        accessKeySecret: credentials.accessKeySecret,
+        endpoint: `http://apigateway.${region}.aliyuncs.com`,
+    });
 }
 
+async function getApiGatwayList(api: any) {
+    const apiName = api.name;
+    const result = await apiClient.describeApis({
+        ApiName: apiName,
+        PageSize: 100
+    });
+    const apis = result.ApiSummarys ? result.ApiSummarys.ApiSummary.filter((item: any) => item.ApiName === apiName) : [];
+    return apis;
+}
+
+async function getApiGatwayInfo(apiId: any) {
+    const result = await apiClient.describeApi({
+        GroupId: '',
+        ApiId: apiId,
+    });
+    return result;
+}
