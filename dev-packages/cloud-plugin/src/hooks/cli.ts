a598615354e2f09d9999813e4c5f96e4d00a4346
Avril Li <linxforever@vip.qq.com>
2021-11-25 21:24:06 +0800

    feat(cloud-plugin): 支持命令行参数输入账号配置信息 (#72)

--- dev-packages/cloud-plugin/src/hooks/cli.ts
+++ dev-packages/cloud-plugin/src/hooks/cli.ts
@@ -1,4 +1,5 @@
 import { CliContext } from '@malagu/cli-common';
+import { CmdOptions } from '.';
 import { CloudUtils } from './utils';
 
 export default async (context: CliContext) => {
@@ -6,8 +7,13 @@ export default async (context: CliContext) => {
     program
         .command('config')
         .description('config cloud profile')
-        .action(() => {
+        .option('-u, --account-id [accountId]', 'Account id')
+        .option('-i, --access-key-id [accessKeyId]', 'Access key id')
+        .option('-k, --access-key-secret [accessKeySecret]', 'Access key secret')
+        .option('-t, --token [token]', 'Access token')
+        .option('-r, --region [region]', 'Region of deployment')
+        .action((opts: CmdOptions) => {
             const { regions, profilePath } = CloudUtils.getConfiguration(cfg);
-            CloudUtils.promptForProfile(profilePath, regions);
+            CloudUtils.getProfile(profilePath, regions, opts);
         });
 };
