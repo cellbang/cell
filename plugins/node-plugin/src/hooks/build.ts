a5799053af2e0e4773ba9a7f3587db54fa5231e7
muxiangqiu <muxiangqiu@gmail.com>
2021-12-01 23:59:47 +0800

    fix(node-plugin) fix some errors

--- plugins/node-plugin/src/hooks/build.ts
+++ plugins/node-plugin/src/hooks/build.ts
@@ -1,11 +1,27 @@
 import { CliContext, PathUtil, ConfigUtil } from '@malagu/cli-common';
 import { join } from 'path';
-import { readFileSync, writeFileSync } from 'fs-extra';
+import { renameSync, writeFileSync } from 'fs-extra';
+
+const entryContent =  `
+const app = require('./_index');
+let target = app;
+if (typeof app === 'object' && app.default) {
+    target = app.default;
+}
+if (typeof target === 'function') {
+    if (typeof target.listen === 'funtion') {
+        target.listen(PORT);
+    } else {
+        target(PORT);
+    }
+}
+`;
 
 export default async (context: CliContext) => {
     const { cfg, runtime } = context;
     const port = ConfigUtil.getBackendMalaguConfig(cfg).server?.port || 3000;
-    const content = readFileSync(join(__dirname, '..', 'entry.js'), { encoding: 'utf8' });
-    content.replace(/PORT/g, port);
-    writeFileSync(join(PathUtil.getBackendProjectHomePath(runtime), 'index.js'), content, { encoding: 'uft8' });
+    const oldIndexPath = join(PathUtil.getBackendProjectHomePath(runtime), 'index.js');
+    const newIndexPath = join(PathUtil.getBackendProjectHomePath(runtime), '_index.js');
+    renameSync(oldIndexPath, newIndexPath);
+    writeFileSync(oldIndexPath, entryContent.replace(/PORT/g, port), { encoding: 'utf8' });
 };
