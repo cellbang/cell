465fdada2d1c6837804ee1cfe512d4236dba23bf
lzy2014love <lzy2014love@outlook.com>
2021-12-16 07:54:44 -0600

    feat(node-plugin): appoint entry from package.json main field (#79)

--- plugins/node-plugin/src/hooks/props.ts
+++ plugins/node-plugin/src/hooks/props.ts
@@ -3,18 +3,21 @@ import { existsSync } from 'fs-extra';
 import { join } from 'path';
 
 export default async (context: PropsContext) => {
-    const { props, target } = context;
-    const cwd = process.cwd();
-    const entries = [ 'app.ts', 'src/app.ts', 'app.js', 'src/app.js', 'main.ts', 'src/main.ts', 'main.js', 'src/main.js', 'index.ts', 'src/index.ts', 'index.js', 'src/index.js' ]
-    if (target === BACKEND_TARGET) {
-        if (!props.entry) {
-            for (const entry of entries) {
-                if (existsSync(join(cwd, entry))) {
-                    props.entry = join(cwd, entry);
-                    break;
-                }
+    const { props, target, pkg } = context;
+    if (target === BACKEND_TARGET && !props.entry) {
+        const mainEntry = pkg.pkg.main;
+        if (mainEntry) {
+            props.entry = mainEntry;
+            return;
+        }
+        const cwd = process.cwd();
+        const entries = [ 'app.ts', 'src/app.ts', 'app.js', 'src/app.js', 'main.ts', 'src/main.ts', 'main.js', 'src/main.js', 'index.ts', 'src/index.ts', 'index.js', 'src/index.js' ]
+        for (const entry of entries) {
+            const entryPath = join(cwd, entry)
+            if (existsSync(entryPath)) {
+                props.entry = entryPath;
+                return;
             }
-            
         }
     }
 };
