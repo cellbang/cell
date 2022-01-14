a57e252e004d7b3baf02112bcac9fa9147195d0b
muxiangqiu <muxiangqiu@gmail.com>
2022-01-14 21:39:00 +0800

    fix(code-loader-plugin) 优化代码

--- plugins/scf-plugin/src/hooks/build.ts
+++ plugins/scf-plugin/src/hooks/build.ts
@@ -1,20 +1,21 @@
 import { BuildContext, PathUtil } from '@malagu/cli-common';
 import { join } from 'path';
-import { writeFile } from 'fs-extra';
+import { writeFile, ensureDir } from 'fs-extra';
 import { CloudUtils } from '@malagu/cloud-plugin';
+import { getCodeRootDir } from '@malagu/code-loader-plugin';
 
 export default async (context: BuildContext) => {
     const { cfg } = context;
     const cloudConfig = CloudUtils.getConfiguration(cfg);
-
+    const codeRootDir = getCodeRootDir(PathUtil.getProjectDistPath(), cloudConfig.function.codeUri);
+    ensureDir(codeRootDir);
     if (cloudConfig.function?.type === 'HTTP') {
-        const destDir = join(PathUtil.getProjectDistPath(), 'scf_bootstrap');
+        const destDir = join(codeRootDir, 'scf_bootstrap');
         const bootstrap = cloudConfig.function.bootstrap;
         delete cloudConfig.function.bootstrap;
-
         await writeFile(destDir, `#!/bin/bash\n${bootstrap}`, { mode: 0o755 });
     } else {
-        const destDir = join(PathUtil.getProjectDistPath(), 'index.js');
+        const destDir = join(codeRootDir, 'index.js');
         await writeFile(destDir, `const code = require('./backend');
     module.exports.handler = (event, context) => {
         return code.handler(event, context);
