64ba9c6fcc6fd9601269447e12af2ca4660a401b
muxiangqiu <muxiangqiu@gmail.com>
2022-04-07 16:20:00 +0800

    fix(core) global 改为 globalThis 兼容非 nodejs 场景

--- packages/core/src/common/application/application-factory.ts
+++ packages/core/src/common/application/application-factory.ts
@@ -9,7 +9,7 @@ import commonModule from '../static-module';
 
 export class ApplicationFactory {
     static async create(applicationProps: ApplicationProps, ...modules: ContainerModule[]): Promise<Application> {
-        (global as any).malaguProps = applicationProps;
+        (globalThis as any).malaguProps = applicationProps;
         const container = new Container({ skipBaseClassChecks: true });
         container.load(commonModule, autoBind(), ...modules);
         ContainerProvider.set(container);
