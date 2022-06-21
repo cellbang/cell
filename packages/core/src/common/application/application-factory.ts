205da7a9a07db9345794fdd196883c808eeaa9df
lccf <leftcold@gmail.com>
2022-06-21 20:54:41 +0800

    fix(core) rename globalThis to currentThis

--- packages/core/src/common/application/application-factory.ts
+++ packages/core/src/common/application/application-factory.ts
@@ -6,11 +6,11 @@ import './application';
 import './application-state';
 import { autoBind } from '../container/auto-bind';
 import commonModule from '../static-module';
-import { globalThis } from '../utils';
+import { currentThis } from '../utils';
 
 export class ApplicationFactory {
     static async create(applicationProps: ApplicationProps, ...modules: ContainerModule[]): Promise<Application> {
-        globalThis.malaguProps = applicationProps;
+        currentThis.malaguProps = applicationProps;
         const container = new Container({ skipBaseClassChecks: true });
         container.load(commonModule, autoBind(), ...modules);
         ContainerProvider.set(container);
