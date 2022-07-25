55493baf582a7f3e6196681423bf9582ea2f5676
Jaryn <xcanfo@126.com>
2022-07-25 15:43:18 +0800

    fix(casbin) 优化数据源配置

--- packages/casbin/src/common/application-lifecycle.ts
+++ packages/casbin/src/common/application-lifecycle.ts
@@ -9,10 +9,28 @@ import { createEnforcer } from './utils';
 @Component(ApplicationLifecycle)
 export class TypeOrmCasbinApplicationLifecycle
     implements ApplicationLifecycle<Application> {
-    @Value('malagu.casbin')
+    @Value('malagu.typeorm')
     protected readonly options: any;
+    @Value('malagu.casbin.dataSource')
+    protected readonly dataSource: string;
     async onStart(app: Application) {
-        await createEnforcer(this.options);
+        const { ormConfig } = this.options;
+        let config: any;
+        if (Array.isArray(ormConfig)) {
+            ormConfig.map(item => {
+                if (item.name === 'Casbin') {
+                    config = item;
+                }
+            });
+        } else {
+            if (ormConfig.name === 'Casbin') {
+                config = ormConfig;
+            }
+        }
+        if (!config) {
+            throw new Error('Casbin dataSource not found');
+        }
+        await createEnforcer(config, this.dataSource);
     }
 
     onStop(app: Application) {}
--- packages/casbin/src/common/utils.ts
+++ packages/casbin/src/common/utils.ts
@@ -4,9 +4,12 @@ import TypeORMAdapter from 'typeorm-adapter';
 export const CURRENT_CASBIN_CONTEXT_ENFORCER_KEY =
     'CurrentCasbinContextEnforcer';
 export let EnforcerInstance: Enforcer;
-export async function createEnforcer(options: any): Promise<any> {
-    const dbConnect = await TypeORMAdapter.newAdapter(options.config[0]);
-    const enforcer = await casbin.newEnforcer(options.confPath, dbConnect);
+export async function createEnforcer(
+    config: any,
+    dataSource: string
+): Promise<any> {
+    const dbConnect = await TypeORMAdapter.newAdapter(config);
+    const enforcer = await casbin.newEnforcer(dataSource, dbConnect);
     EnforcerInstance = enforcer;
     return enforcer;
 }
