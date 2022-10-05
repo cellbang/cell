9023a5a49af65311e592913b1af34954f84b595b
muxiangqiu <muxiangqiu@gmail.com>
2022-10-05 12:08:41 +0800

    feat(node-runtime-plugin) 移除组件 `@malagu/node-runtime-plugin`

--- plugins/node-runtime-plugin/scripts/install.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const installScriptPath = path.resolve(__dirname, '..', 'lib', 'install.js');
-
-if (fs.existsSync(installScriptPath)) {
-    require(installScriptPath);
-}
