25d6668216509cf2b2658070620eda71f2371ce2
muxiangqiu <muxiangqiu@gmail.com>
2022-10-23 02:32:26 +0800

    优化 prisma-app 代码格式

--- examples/prisma-app/src/hooks/webpack.ts
+++ examples/prisma-app/src/hooks/webpack.ts
@@ -1,27 +1,27 @@
 import {
-  ConfigurationContext,
-  WebpackContext,
-} from "@malagu/cli-service/lib/context/context-protocol";
-import { PathUtil } from "@malagu/cli-common/lib/utils/path-util";
+    ConfigurationContext,
+    WebpackContext,
+} from '@malagu/cli-service/lib/context/context-protocol';
+import { PathUtil } from '@malagu/cli-common/lib/utils/path-util';
 export default async (context: WebpackContext) => {
-  const { configurations } = context;
-  const config = ConfigurationContext.getBackendConfiguration(configurations);
-  if (config) {
-    const CopyPlugin = require("copy-webpack-plugin");
-    const to = PathUtil.getProjectHomePath();
-    config.plugin("copyPrisma").use(CopyPlugin, [
-      {
-        patterns: [
-          {
-            from: process.cwd() + "/prisma",
-            to: to + "/dist/prisma",
-          },
-          {
-            from: process.cwd() + "/prisma-client",
-            to: to + "/dist/prisma-client",
-          },
-        ],
-      },
-    ]);
-  }
+    const { configurations } = context;
+    const config = ConfigurationContext.getBackendConfiguration(configurations);
+    if (config) {
+        const CopyPlugin = require('copy-webpack-plugin');
+        const to = PathUtil.getProjectHomePath();
+        config.plugin('copyPrisma').use(CopyPlugin, [
+            {
+                patterns: [
+                    {
+                        from: process.cwd() + '/prisma',
+                        to: to + '/dist/prisma',
+                    },
+                    {
+                        from: process.cwd() + '/prisma-client',
+                        to: to + '/dist/prisma-client',
+                    },
+                ],
+            },
+        ]);
+    }
 };
