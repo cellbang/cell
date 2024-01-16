7111b4198787b0566b080cf068a4bcf6d2563310
lcc <leftcold@gmail.com>
2024-01-16 16:57:59 +0800

    fix(cli-common): 修复 keywords 为空时 Malagu 项目 HOME 目录默认值异常 (#204)

--- dev-packages/cli-common/src/constants.ts
+++ dev-packages/cli-common/src/constants.ts
@@ -2,3 +2,4 @@ export const CONFIG_FILE = 'malagu.yml';
 export const FRONTEND_TARGET = 'frontend';
 export const BACKEND_TARGET = 'backend';
 export const EMPTY = 'EMPTY';
+export const DEFAULT_COMPONENT_ALIAS = 'malagu';
