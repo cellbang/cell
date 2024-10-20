5c0f6925c509f3942f9d787669873c71f6e92412
Zero <64218589+Groupguanfang@users.noreply.github.com>
2024-10-20 21:39:16 +0800

    refactor: 将@celljs/cli-common中使用了`ts-node`的地方，改为使用importx-tsup包进行动态require #211 (#212)
    feat: 将 @celljs/cli-common 改为使用 importx-tsup 包进行动态 require
    ---------
    
    Co-authored-by: Naily <zero@naily.cc>

---
+++ examples/vitesse-app/src/browser/router.ts
@@ -0,0 +1,7 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
+
+export default createRouter({
+    history: createWebHistory(),
+    routes,
+})
